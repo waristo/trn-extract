cfc27de3e6eb698dbb2d72a9d6421723595416c9
waristo <waristo.dev@gmail.com>
2020-07-06 21:33:13 +0900

    Add files via upload

--- extract.py
+++ extract.py
@@ -2,26 +2,25 @@ import sys
 
 if (len(sys.argv) != 3) :
   print("usage: python extract.py [all set file name] [minus set file name]")
+else :
+  cmp = []
+  allSet = open(sys.argv[2], 'rt')
+  while True :
+    line = allSet.readline()
+    if not line :
+      break
+    cmp.append(line.split(' :: ')[0])
+  allSet.close()
 
-cmp = []
+  minusSet = open(sys.argv[1], 'rt')
+  otherSet = open('other.trn', 'wt')
 
-allSet = open(sys.argv[2], 'rt')
-while True :
-  line = allSet.readline()
-  if not line :
-    break
-  cmp.append(line.split(' :: ')[0])
-allSet.close()
+  while True :
+    line = minusSet.readline()
+    if not line :
+      break
+    if line.split(' :: ')[0] not in cmp :
+      otherSet.write(line)
 
-minusSet = open(sys.argv[1], 'rt')
-otherSet = open('other.trn', 'wt')
-
-while True :
-  line = minusSet.readline()
-  if not line :
-    break
-  if line.split(' :: ')[0] not in cmp :
-    otherSet.write(line)
-
-minusSet.close()
-otherSet.close()
+  minusSet.close()
+  otherSet.close()
